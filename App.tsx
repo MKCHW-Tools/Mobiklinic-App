53df04f615ae6dc8fff2523ce540993bc5d2e7ff
Arafat <sp@SPs-MacBook-Pro.local>
2023-05-10 15:20:25 +0300

    connecting to activity using intents

--- App.tsx
+++ App.tsx
@@ -11,17 +11,15 @@ import {
   Linking,
   Button,
   Alert,
+  Platform,
   StyleSheet,
   useColorScheme,
+  NativeModules,
   View,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  Colors
 } from 'react-native/Libraries/NewAppScreen';
 
 
@@ -35,27 +33,74 @@ type SendIntentButtonProps = {
 };
 
 
-const SendIntentButton = ({
-  action,
-  extras,
-  children,
-}: SendIntentButtonProps) => {
-  const handlePress = useCallback(async () => {
-    try {
-      await Linking.sendIntent(action, extras);
-    } catch (e: any) {
-      Alert.alert(e.message);
-    }
-  }, [action, extras]);
+// const SendIntentButton = ({
+//   action,
+//   extras,
+//   children,
+// }: SendIntentButtonProps) => {
+//   const handlePress = useCallback(async () => {
+//     try {
+//       await Linking.sendIntent(action, extras);
+//     } catch (e: any) {
+//       Alert.alert(e.message);
+//     }
+//   }, [action, extras]);
 
-  return <Button title={children} onPress={handlePress} />;
-};
+//   return <Button title={children} onPress={handlePress} />;
+// };
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
+// const OpenSimprintsButton = () => {
+
+//   const handlePress = useCallback(async () => {
+//     try {
+//       await Linking.openURL('https://play.google.com/store/apps/details?id=com.simprints.id');
+//     } catch (e: any) {
+//       Alert.alert(e.message);
+//     }
+//   }, []);
+
+//   return <Button title="Open Simprints in playstore" onPress={handlePress} />;
+// };
+
+
+// function launchSimprintsID() {
+//   // Check if the device is running Android
+//   if (Platform.OS === 'android') {
+//     // Construct the intent URI
+//     const uri = 'intent://app/#Intent;scheme=com.simprints.id;end';
+
+//     // Open the intent URI using the Linking module
+//     Linking.openURL(uri).catch((err) => {
+//       console.error('Failed to open Simprints ID app:', err);
+//     });
+//   } else {
+//     console.error('The Simprints ID app is only available on Android.');
+//   }
+// }
+
+// function MyComponent() {
+//   // Call the launchSimprintsID function when a button is pressed
+//   const handleButtonPress = () => {
+//     launchSimprintsID();
+//   };
+
+//   return (
+//     <Button title="Launch Simprints ID" onPress={handleButtonPress} />
+//   );
+// }
+
+
 function App(): JSX.Element {
+  var OpenActivity = NativeModules.OpenActivity;
+
+  const openFunction = () => {
+    OpenActivity.open();
+  };
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -64,10 +109,19 @@ function App(): JSX.Element {
 
   return (
     <View style={styles.container}>
-      <SendIntentButton action="android.intent.action.simprints.id">
+      {/* <SendIntentButton action="com.android.intent.chrome">
         Hello SimPrints
       </SendIntentButton>
-    
+
+      <SendIntentButton action="android.intent.action.POWER_USAGE_SUMMARY">
+        Power Usage Summary
+      </SendIntentButton> */}
+
+      {/* <OpenSimprintsButton />
+
+      <MyComponent /> */}
+
+      <Button title="Open Native Activity" onPress={openFunction} />
     </View>
   );
 };
--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -9,7 +9,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
-import com.burnweb.rnsendintent.RNSendIntentPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new RNSendIntentPackage());
+          packages.add(new OpenActivityPackage());
           return packages;
         }
 
