5903db0f958da1294c3eca29024be14f0988f4da
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-05-17 11:49:11 +0300

    AppStack Component

---
+++ components/navigators/AppNav.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AppStack from "./AppStack";
+import AuthStack from "./AuthStack";
+import { AuthContext } from "../contexts/auth";
+function AppNav() {
+	const { user } = useContext(AuthContext);
+	return (
+		<NavigationContainer>
+			{user ? <AuthStack /> : <AppStack />}
+		</NavigationContainer>
+	);
+}
+
+export default AppNav;
