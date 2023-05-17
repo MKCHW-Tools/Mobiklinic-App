19005adc08f2105a52bbe6edb4afb27bdb65667a
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-05-17 13:58:47 +0300

    react native component

---
+++ components/screens/diagnosis.js
@@ -0,0 +1,240 @@
+import * as React from "react";
+import {
+	View,
+	FlatList,
+	TouchableOpacity,
+	Text,
+	StyleSheet,
+	StatusBar,
+} from "react-native";
+
+import Loader from "../ui/loader";
+import { ListItem } from "react-native-elements";
+import { CustomStatusBar } from "../ui/custom.status.bar";
+import Icon from "react-native-vector-icons/Feather";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { DiagnosisContext } from "../providers/Diagnosis";
+
+import { COLORS, DIMENS } from "../constants/styles";
+
+import CustomHeader from "../parts/custom-header";
+
+const Diagnose = ({ navigation }) => {
+	const diagnosesContext = React.useContext(DiagnosisContext);
+	const { diagnoses } = diagnosesContext;
+
+	const [state, setState] = React.useState({ isLoading: true });
+
+	const _keyExtractor = (item, index) => index.toString();
+
+	const _renderItem = ({ item }) => {
+		return (
+			<TouchableOpacity
+				onPress={() => navigation.navigate("ViewDiagnosis", item)}>
+				<ListItem bottomDivider>
+					{item?.patient?.gender == "Female" ? (
+						item?.patient?.isPregnant ? (
+							<Icon
+								name="check-circle"
+								color={COLORS.PRIMARY}
+								size={20}
+							/>
+						) : (
+							<Icon
+								name="minus-circle"
+								color={COLORS.GREY}
+								SIZE={25}
+							/>
+						)
+					) : (
+						<Icon name="circle" color={COLORS.GREY} SIZE={25} />
+					)}
+
+					<ListItem.Content>
+						<ListItem.Title style={STYLES.listTitle}>
+							{item?.code}
+						</ListItem.Title>
+
+						<ListItem.Subtitle style={STYLES.subtitle}>
+							<Text>Age {item?.patient?.age}</Text>
+						</ListItem.Subtitle>
+					</ListItem.Content>
+					<ListItem.Chevron size={30} />
+				</ListItem>
+			</TouchableOpacity>
+		);
+	};
+
+	React.useEffect(() => {
+		// AsyncStorage.removeItem("@diagnosis");
+		getDiagnoses();
+	}, []);
+
+	const getDiagnoses = async () => {
+		try {
+			const jsonString = await AsyncStorage.getItem("@diagnosis");
+
+			diagnosesContext.setDiagnoses(
+				jsonString ? JSON.parse(jsonString) : diagnoses
+			);
+			setState({ ...state, isLoading: false });
+		} catch (err) {
+			throw err;
+		}
+	};
+
+	const _header = () => (
+		<CustomHeader
+			left={
+				<TouchableOpacity
+					style={{
+						marginHorizontal: 4,
+						width: 35,
+						height: 35,
+						/*                         borderRadius: 100,
+                        backgroundColor: COLORS.GREY,
+                        borderColor:COLORS.BLACK,
+                        borderStyle:'solid',
+                        borderWidth:1, */
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+					onPress={() => navigation.openDrawer()}>
+					<Icon name="menu" size={25} color={COLORS.BLACK} />
+				</TouchableOpacity>
+			}
+			title={
+				<Text style={[STYLES.centerHeader, STYLES.title]}>
+					Diagnoses
+				</Text>
+			}
+		/>
+	);
+
+	if (state.isLoading) {
+		return <Loader />;
+	} else {
+		if (typeof diagnoses === "object" && diagnoses.length == 0)
+			return (
+				<View style={STYLES.wrapper}>
+					<CustomStatusBar />
+
+					{_header()}
+
+					<View style={STYLES.body}>
+						<Icon name="smile" size={60} color={COLORS.GREY} />
+
+						<Text style={STYLES.alert}>
+							Can not find Diagnosis Info to Display.
+						</Text>
+					</View>
+					<TouchableOpacity
+						onPress={() => navigation.navigate("NewDiagnosis")}
+						style={{
+							marginHorizontal: 10,
+							flexDirection: "row",
+							borderRadius: 100,
+							padding: 10,
+							paddingHorizontal: 20,
+							backgroundColor: COLORS.BLACK,
+							justifyContent: "space-between",
+							alignItems: "center",
+							marginBottom: 20,
+						}}>
+						<Text style={STYLES.textBtn}>Add New</Text>
+						<Icon name="plus" size={25} color={COLORS.WHITE} />
+					</TouchableOpacity>
+				</View>
+			);
+
+		return (
+			<View style={STYLES.wrapper}>
+				<CustomStatusBar />
+
+				{_header()}
+
+				<FlatList
+					data={diagnoses}
+					renderItem={_renderItem}
+					keyExtractor={_keyExtractor}
+				/>
+				<TouchableOpacity
+					onPress={() => navigation.navigate("NewDiagnosis")}
+					style={{
+						marginHorizontal: 10,
+						flexDirection: "row",
+						//height: 50,
+						borderRadius: 100,
+						padding: 10,
+						paddingHorizontal: 20,
+						backgroundColor: COLORS.BLACK,
+						justifyContent: "space-between",
+						alignItems: "center",
+						marginBottom: 20,
+					}}>
+					<Text style={STYLES.textBtn}>Add New</Text>
+					<Icon name="plus" size={25} color={COLORS.WHITE} />
+				</TouchableOpacity>
+			</View>
+		);
+	}
+};
+
+const STYLES = StyleSheet.create({
+	wrapper: {
+		flex: 1,
+		backgroundColor: COLORS.SECONDARY,
+	},
+	header: {
+		flex: 1,
+	},
+	body: {
+		flex: 2,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	title: {
+		fontWeight: "bold",
+		color: COLORS.SECONDARY,
+		textAlign: "center",
+	},
+	alert: {
+		color: COLORS.GREY,
+		textAlign: "center",
+		marginTop: 15,
+	},
+	listTitle: {
+		color: COLORS.BLACK,
+		fontSize: 16,
+		fontWeight: "bold",
+	},
+	subtitle: {
+		flexDirection: "row",
+		// opacity:0.5
+	},
+	label: {
+		fontWeight: "bold",
+		marginRight: 5,
+	},
+	leftHeader: {
+		flex: 1,
+		paddingLeft: 10,
+	},
+	centerHeader: {
+		flex: 2,
+		flexDirection: "row",
+	},
+	yesText: {
+		color: COLORS.PRIMARY,
+	},
+	rightHeader: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "flex-end",
+	},
+	textBtn: {
+		color: COLORS.WHITE,
+	},
+});
+
+export default Diagnose;
