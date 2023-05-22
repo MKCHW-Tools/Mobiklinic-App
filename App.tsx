e6343574fc50e232324ba91d61cc038ee78d4097
Arafat <sp@SPs-MacBook-Pro.local>
2023-05-23 01:01:53 +0300

    Add results

--- App.tsx
+++ App.tsx
@@ -1,10 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Button, NativeModules, NativeEventEmitter } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useCallback, useEffect, useState } from 'react';
+import type { PropsWithChildren } from 'react';
+import { DeviceEventEmitter, NativeEventEmitter, Text } from 'react-native';
+// import BeneficiarySelectionScreen from './BeneficiarySelectionScreen';
+
+import {
+  Linking,
+  Button,
+  Alert,
+  Modal,
+  StyleSheet,
+  useColorScheme,
+  NativeModules,
+  View,
+} from 'react-native';
 
 const { IdentificationModule } = NativeModules;
 const identificationEventEmitter = new NativeEventEmitter(IdentificationModule);
 
-const MyComponent = () => {
+
+DeviceEventEmitter.addListener('SimprintsRegistrationSuccess', (event) => {
+  const { guid } = event;
+  console.log(event);
+  Alert.alert("Simprints Registration Success", guid);
+}
+);
+
+DeviceEventEmitter.addListener('SimprintsRegistrationFailure', (event) => {
+  const { error } = event;
+  Alert.alert("Simprints Registration Failure", error);
+}
+);
+
+
+
+function App(): JSX.Element {
   const [identificationResults, setIdentificationResults] = useState([]);
 
   useEffect(() => {
@@ -28,17 +64,47 @@ const MyComponent = () => {
     IdentificationModule.triggerIdentification(projectID, moduleID, userID);
   };
 
+
+  var OpenActivity = NativeModules.OpenActivity;
+
+  const openFunction = () => {
+    OpenActivity.open("WuDDHuqhcQ36P2U9rM7Y", "test_user", "mpower");
+  };
+  
+
+
   return (
-    <View>
+    <View style={styles.container}>
+      <View>
+      <Button title="Start Enorollment" onPress={openFunction} />
+      <View style={{ height: 20 }} />
+      <Button title="Start Identification" onPress={handleIdentification} />
       <Text>Identification Results:</Text>
       {(identificationResults as any[]).map((result) => (
         <Text key={result.guid}>
           Tier: {result.tier}, Confidence: {result.confidenceScore}, Guid: {result.guid}
         </Text>
       ))}
-      <Button title="Start Identification" onPress={handleIdentification} />
+      </View>
     </View>
   );
 };
 
-export default MyComponent;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 20,
+  },
+});
+
+
+
+export default App;
