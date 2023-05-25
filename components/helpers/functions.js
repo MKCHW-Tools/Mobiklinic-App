9f34814e89cb550008f913d661acf28b3962a55f
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-05-25 12:17:07 +0300

    changing functions

--- components/helpers/functions.js
+++ components/helpers/functions.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 import {URLS} from '../constants/API';
 import uniqWith from 'lodash/uniqWith';
 import isEqual from 'lodash/isEqual';
+import {CommonActions} from '@react-navigation/native';
 
 export const _removeStorageItem = async key => {
   return await AsyncStorage.removeItem(key);
@@ -30,37 +31,65 @@ export const MyDate = () => {
   }-${myDate.getDate()}-${myDate.getFullYear()} ${myDate.getHours()}:${myDate.getMinutes()}:${myDate.getSeconds()}`;
 };
 
-export const tokensRefresh = async () => {
-  const Tokens = await AsyncStorage.getItem('tokens');
-  // console.log(Tokens);
-  try {
-    const {refreshToken: rToken} = JSON.parse(Tokens);
-    console.log(refreshToken);
+export const tokensRefresh = async user => {
+  const refresh = user.tokens.refresh;
 
-    const response = await fetch(`${URLS.BASE}/tokens/refresh`, {
+  try {
+    const response = fetch(`${URLS.BASE}/tokens/refresh`, {
       method: 'GET',
       headers: {
-        Authorization: 'Bearer ' + rToken,
+        Authorization: `Bearer ${refresh}`,
         'Content-type': 'application/json; charset=UTF-8',
         Accept: 'application/json',
       },
     });
 
-    const JSON_RESPONSE = await response.json();
+    const data = await response.then(async response => {
+      if (response.status === 401) {
+        // the refresh token no longer works
+        await AsyncStorage.setItem(
+          '@user',
+          JSON.stringify({
+            id: user.id,
+            username: user.username,
+            hash: null, // overwrite the hash so that the user surely try to obtain new refresh tokens when they sign in next time.
+            tokens: user.tokens,
+          }),
+        );
+        return null;
+      }
+      return response.json();
+    });
+    if (data === null) return null;
 
-    const {accessToken, refreshToken, msg, result} = JSON_RESPONSE;
-
-    if (result === 'Failure') return false;
-
+    const {accessToken, refreshToken, msg, result} = data;
     if (result === 'Success') {
+      await SAVE_LOCAL_USER({
+        id: user.id,
+        username: user.username,
+        password: user.password,
+        tokens: {
+          access: accessToken,
+          refresh: refreshToken,
+        },
+        offline: user.offline,
+      });
+
       return {
-        accessToken,
-        refreshToken,
+        id: user.id,
+        username: user.username,
+        tokens: {
+          access: accessToken,
+          refresh: refreshToken,
+        },
+        offline: user.offline,
       };
     }
   } catch (e) {
     console.error(e.message);
   }
+
+  return null;
 };
 
 export const RETRIEVE_LOCAL_USER = async () => {
@@ -167,7 +196,6 @@ export const signIn = async data => {
 
   try {
     theUser = await RETRIEVE_LOCAL_USER();
-    // theUser = JSON.parse(theUser);
   } catch (err) {
     console.log(err);
   }
@@ -184,97 +212,120 @@ export const signIn = async data => {
         offline: true,
       });
       setIsLoading(false);
-      // console.log(myUser);
-      // setTokens({ access: hash });
       return;
-    } else {
-      Alert.alert('Failed to login', 'Try again, please!');
-      return;
-    }
-  } else {
-    try {
-      console.log('Starting network request');
-      let response = await fetch(`${URLS.BASE}/users/login`, {
-        method: 'POST',
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-          Accept: 'application/json',
-        },
-      });
-
-      let json_data = await response.json();
-      const {result, id, accessToken, refreshToken} = json_data;
-
-      if (result == 'Success') {
-        await SAVE_LOCAL_USER({
-          id,
-          username,
-          password,
-          tokens: {access: accessToken, refresh: refreshToken},
-        });
-
-        const resources = ['ambulances', 'doctors', 'diagnosis'];
-
-        if (
-          DOWNLOAD({
-            accessToken,
-            items: resources,
-            per_page: 10,
-          })
-        ) {
-          setUser({
-            id,
-            username,
-            tokens: {access: accessToken, refresh: refreshToken},
-            offline: false,
-          });
-          setIsLoading(false);
-          // setTokens({ access: accessToken });
-        }
-      } else
-        Alert.alert(
-          'Failed to login',
-          'Check your login details',
-          [
-            {
-              text: 'Cancel',
-              onPress: () => setIsLoading(false),
-            },
-          ],
-
-          {
-            cancelable: true,
-            onDissmiss: () => {
-              setIsLoading(false);
-            },
-          },
-        );
-    } catch (err) {
-      err?.message == 'Network request failed' &&
-        Alert.alert(
-          'Oops!',
-          'Check your internet connection',
-          [
-            {
-              text: 'Cancel',
-              onPress: () => setIsLoading(false),
-            },
-          ],
-          {
-            cancelable: true,
-            onDissmiss: () => {
-              setIsLoading(false);
-            },
-          },
-        );
-      setIsLoading(false);
-      console.log(err);
+      //// It is possible that the user has changed the password, but it adheres to the past information stored on the device.
+      //// This, we need to ask the online server when the user fail to sign in with the stored information.
+      ////
+      // } else {
+      // 	Alert.alert(
+      // 		"Failed to login",
+      // 		"Check your login details",
+      // 		[
+      // 			{
+      // 				text: "Cancel",
+      // 				onPress: () => setIsLoading(false),
+      // 			},
+      // 		],
+      //
+      // 		{
+      // 			cancelable: true,
+      // 			onDismiss: () => {
+      // 				setIsLoading(false);
+      // 			},
+      // 		}
+      // 	);
     }
   }
+  // } else {
+  try {
+    console.log('Logging in user ' + username);
+    let response = await fetch(`${URLS.BASE}/users/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        Accept: 'application/json',
+      },
+    });
+
+    let json_data = await response.json();
+    const {result, id, accessToken, refreshToken} = json_data;
+
+    if (result == 'Success') {
+      await SAVE_LOCAL_USER({
+        id,
+        username,
+        password,
+        tokens: {access: accessToken, refresh: refreshToken},
+      });
+
+      const resources = ['ambulances', 'doctors', 'diagnosis'];
+
+      if (
+        DOWNLOAD({
+          accessToken,
+          items: resources,
+          per_page: 10,
+        })
+      ) {
+        setUser({
+          id,
+          username,
+          tokens: {access: accessToken, refresh: refreshToken},
+          offline: false,
+        });
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{name: 'Dashboard'}],
+          }),
+        );
+        setIsLoading(false);
+        // setTokens({ access: accessToken });
+      }
+    } else {
+      Alert.alert(
+        'Failed to login',
+        'Check your login details',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => setIsLoading(false),
+          },
+        ],
+        {
+          cancelable: true,
+          onDismiss: () => {
+            setIsLoading(false);
+          },
+        },
+      );
+    }
+  } catch (err) {
+    err?.message == 'Network request failed' &&
+      Alert.alert(
+        'Oops!',
+        'Check your internet connection',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => setIsLoading(false),
+          },
+        ],
+        {
+          cancelable: true,
+          onDismiss: () => {
+            setIsLoading(false);
+          },
+        },
+      );
+    setIsLoading(false);
+    console.log(err);
+  }
+  // }
 };
 
 export const signUp = async data => {
@@ -282,48 +333,62 @@ export const signUp = async data => {
   // We will also need to handle errors if sign up failed
   // After getting token, we need to persist the token using `AsyncStorage`
   // In the example, we'll use a dummy token
-  const {firstname, lastname, theemail, thephone, thepassword, thepassword2} =
-    data.errors;
+  /* 	const {
+		firstname,
+		lastname,
+		theemail,
+		thephone,
+		thepassword,
+		thepassword2,
+	} = data.errors; */
 
-  if (
-    firstname ||
-    lastname ||
-    theemail ||
-    thephone ||
-    thepassword ||
-    thepassword2
-  ) {
-    Alert.alert('Fail', 'Errors, Fix errors in the form, and try again!');
-    return;
-  }
+  /* 	if (
+		firstname ||
+		lastname ||
+		theemail ||
+		thephone ||
+		thepassword ||
+		thepassword2
+	) {
+		Alert.alert("Fail", "Errors, Fix errors in the form, and try again!");
+		return;
+	} */
+  const {
+    firstName,
+    lastName,
+    phoneNumber,
+    password,
+    cPassword,
+    eMail,
+    setIsLoading,
+    setProcess,
+    setRegistered,
+  } = data;
 
-  const {firstName, lastName, phone, password, password2, email} = data;
+  setProcess('Registering, please wait!');
 
   if (
     firstName == '' ||
     lastName == '' ||
-    phone.length < 12 ||
+    phoneNumber.length < 12 ||
     password == '' ||
-    password2 == ''
+    cPassword == ''
   ) {
     Alert.alert('Fail', 'Fix errors in the form, and try again!');
-    console.log(firstName, lastName, phone, email, password, password2);
+    console.log(firstName, lastName, phoneNumber, eMail, password, cPassword);
     return;
   }
 
-  userContext.setIsRegistering('yes');
-
   try {
-    await fetch(`${URLS.BASE}/register`, {
+    await fetch(`${URLS.BASE}/users`, {
       method: 'POST',
       body: JSON.stringify({
-        phone: phone,
-        fname: firstName,
-        lname: lastName,
-        email: email,
+        username: phoneNumber,
+        phone: phoneNumber,
+        name: `${firstName} ${lastName}`,
+        email: eMail,
         role_id: 2,
         password: password,
-        password_confirmation: password2,
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
@@ -332,26 +397,18 @@ export const signUp = async data => {
     })
       .then(res => res.json())
       .then(response => {
-        if (response.result == 'success') {
-          try {
-            userContext.setIsRegistering('complete');
-          } catch (err) {
-            console.error(err);
-          }
-        } else if (response.result == 'failure') {
-          const {email, phone} = response.message;
-
-          if (typeof email != 'undefined')
-            Alert.alert(
-              'Registration failed!',
-              'The email address you entered was used by another member.',
-            );
-          else if (typeof phone != 'undefined')
-            Alert.alert(
-              'Registration failed!',
-              'The phone number you entered was used by another member.',
-            );
-          else Alert.alert('Ooops!', 'Try again!');
+        setIsLoading(false);
+        if (response.result == 'Success') {
+          /* 					Alert.alert(
+						"Registered successfully",
+						"Press Okay to login!"
+					); */
+          setRegistered(true);
+        } else if (response.result == 'Failure') {
+          Alert.alert(
+            'Sign up failure.',
+            'Check Phone number and E-mail. Press Ok to try again.',
+          );
 
           console.log(response);
         } else {
@@ -359,7 +416,7 @@ export const signUp = async data => {
         }
       })
       .catch(err => {
-        console.log(err);
+        console.log(err.message);
         Alert.alert(
           'Failure',
           'Something wrong happened. Check your internet and try again!',
@@ -370,7 +427,7 @@ export const signUp = async data => {
   }
 };
 
-export const signOut = callback => {
+export const signOut = async callback => {
   callback();
 };
 
--- components/screens/login.js
+++ components/screens/login.js
@@ -12,13 +12,212 @@ import {
 
 import Icon from 'react-native-vector-icons/Feather';
 import {COLORS, DIMENS} from '../constants/styles';
-import {signIn} from '../helpers/functions';
+import {URLS} from '../constants/API';
 
 import {AuthContext} from '../contexts/auth';
 import {CustomStatusBar} from '../ui/custom.status.bar';
 import Loader from '../ui/loader';
 
-const Login = () => {
+const RETRIEVE_LOCAL_USER = async () => {
+  try {
+    let user = await AsyncStorage.getItem('@user');
+    return JSON.parse(user) || null;
+  } catch (err) {
+    new Error(err);
+  }
+};
+
+const SAVE_LOCAL_USER = async (user = {}) => {
+  try {
+    const HASH = cyrb53(user.password);
+
+    await AsyncStorage.setItem(
+      '@user',
+      JSON.stringify({
+        id: user.id,
+        username: user.username,
+        hash: HASH,
+        tokens: user.tokens,
+      }),
+    );
+  } catch (err) {
+    new Error(err);
+  }
+};
+
+const cyrb53 = function (str, seed = 0) {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
+
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+
+  h1 =
+    Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^
+    Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 =
+    Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^
+    Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
+const signIn = async data => {
+  //clearStorage();
+  let {user, setIsLoading, setMyUser: setUser} = data;
+
+  if (typeof user === undefined) {
+    Alert.alert('Error', 'Provide your phone number and password');
+    return;
+  }
+
+  const {username, password} = user;
+  let hash = cyrb53(password);
+
+  if (username === '' && password === '') {
+    Alert.alert('Error', 'Provide your phone number and password');
+    return;
+  }
+
+  let theUser = null;
+
+  try {
+    theUser = await RETRIEVE_LOCAL_USER();
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (theUser !== null) {
+    let myUser =
+      theUser.username === username && theUser.hash === hash ? theUser : null;
+
+    if (myUser) {
+      setUser({
+        id: myUser.id,
+        username: myUser.username,
+        tokens: myUser.tokens,
+        offline: true,
+      });
+      setIsLoading(false);
+      return;
+      //// It is possible that the user has changed the password, but it adheres to the past information stored on the device.
+      //// This, we need to ask the online server when the user fail to sign in with the stored information.
+      ////
+      // } else {
+      // 	Alert.alert(
+      // 		"Failed to login",
+      // 		"Check your login details",
+      // 		[
+      // 			{
+      // 				text: "Cancel",
+      // 				onPress: () => setIsLoading(false),
+      // 			},
+      // 		],
+      //
+      // 		{
+      // 			cancelable: true,
+      // 			onDismiss: () => {
+      // 				setIsLoading(false);
+      // 			},
+      // 		}
+      // 	);
+    }
+  }
+  // } else {
+  try {
+    console.log('Logging in user ' + username);
+    let response = await fetch(`${URLS.BASE}/users/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        Accept: 'application/json',
+      },
+    });
+
+    let json_data = await response.json();
+    const {result, id, accessToken, refreshToken} = json_data;
+
+    if (result == 'Success') {
+      await SAVE_LOCAL_USER({
+        id,
+        username,
+        password,
+        tokens: {access: accessToken, refresh: refreshToken},
+      });
+
+      const resources = ['ambulances', 'doctors', 'diagnosis'];
+
+      if (
+        DOWNLOAD({
+          accessToken,
+          items: resources,
+          per_page: 10,
+        })
+      ) {
+        setUser({
+          id,
+          username,
+          tokens: {access: accessToken, refresh: refreshToken},
+          offline: false,
+        });
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{name: 'Dashboard'}],
+          }),
+        );
+        setIsLoading(false);
+        // setTokens({ access: accessToken });
+      }
+    } else {
+      Alert.alert(
+        'Failed to login',
+        'Check your login details',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => setIsLoading(false),
+          },
+        ],
+        {
+          cancelable: true,
+          onDismiss: () => {
+            setIsLoading(false);
+          },
+        },
+      );
+    }
+  } catch (err) {
+    err?.message == 'Network request failed' &&
+      Alert.alert(
+        'Oops!',
+        'Check your internet connection',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => setIsLoading(false),
+          },
+        ],
+        {
+          cancelable: true,
+          onDismiss: () => {
+            setIsLoading(false);
+          },
+        },
+      );
+    setIsLoading(false);
+    console.log(err);
+  }
+  // }
+};
+
+const Login = ({navigation}) => {
   const {
     setUser: setMyUser,
     isLoading,
@@ -76,7 +275,6 @@ const Login = () => {
               signIn({
                 user,
                 setIsLoading,
-                setMyUser,
               });
             }}>
             <Text style={styles.whiteText}>Sign in</Text>
