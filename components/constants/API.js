ba3ec4f4126dcd73d908e8cb8d39c32f6a8d4926
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-05-27 16:23:43 +0300

    using old api to query data

--- components/constants/API.js
+++ components/constants/API.js
@@ -1,5 +1,5 @@
 export const URLS = {
 	BASE: "https://api.mobiklinic.com",
-	// BASE:'https://staging.mobiklinic.com/',
+	// BASE:'https://staging.mobiklinic.com/graphql',
 	LOCAL: "http://192.168.100.6:4600",
 };
