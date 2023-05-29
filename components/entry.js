def15aa91f09a3f4da6b24aeb8d68b3bdf9b45af
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-05-29 14:07:06 +0300

    navigation

--- components/entry.js
+++ components/entry.js
@@ -1,21 +1,28 @@
-import * as React from 'react';
-import {AuthProvider} from './contexts/auth';
+import * as React from "react";
+import { AuthProvider } from "./contexts/auth";
 
-import AppNav from './navigators/AppNav';
-import AuthStack from './navigators/AuthStack';
+import AppNav from "./navigators/AppNav";
 
 const Entry = () => {
-  React.useEffect(() => {
-    //clearStorage();
-    //autoLogin()
-  }, []);
-  return (
-    <AuthProvider>
-    	<AppNav />
-    </AuthProvider>
+	React.useEffect(() => {
+		//clearStorage();
+		//autoLogin()
+	}, []);
 
-    // <AuthStack />
-  );
+	// Check if the user is logged in,
+	// if yes, navigate to the app
+	// if no, navigate to the login screen
+	// To login the user, we need to send the user's credentials to the server
+	// and get a token back
+	//If network error, retrieve users from local storage
+	// Compare users to the logged in user
+	// If the user is found, login the user and navigate to the app.
+	// If the user is not found, show an error message
+	return (
+		<AuthProvider>
+			<AppNav />
+		</AuthProvider>
+	);
 };
 
 export default Entry;
