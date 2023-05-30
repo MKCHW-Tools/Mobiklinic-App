debe25285bff9dabb4c646bbfcc6f015ac857cc2
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-05-30 13:30:13 +0300

    medical data

--- components/simprints/patient.medical.js
+++ components/simprints/patient.medical.js
@@ -1,9 +1,425 @@
-import React from 'react'
+import * as React from 'react';
+import {
+  View,
+  Alert,
+  Switch,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Button,
+} from 'react-native';
 
-const MedicalHistory = () => {
+import {Picker} from '@react-native-picker/picker';
+import Icon from 'react-native-vector-icons/Feather';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import {COLORS, DIMENS} from '../constants/styles';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {
+  _removeStorageItem,
+  generateRandomCode,
+  MyDate,
+} from '../helpers/functions';
+
+import {DiagnosisContext} from '../providers/Diagnosis';
+import CustomHeader from '../ui/custom-header';
+import Loader from '../ui/loader';
+
+const PatientMedical = ({navigation}) => {
+  const diagnosisContext = React.useContext(DiagnosisContext);
+  const {diagnoses} = diagnosisContext;
+
+  // date
+  const currentDate = new Date();
+
+  const [state, setState] = React.useState({
+    isLoading: false,
+    labResults: '',
+    existingMedical:'',
+    currentMedication: '',
+    phone: '',
+    condition: '',
+    sampleCollection:'',
+    diagnoseDate: new Date(),
+    diagnosises: [],
+  });
+
+  const save = async () => {
+    const {
+      currentMedication,
+      existingMedical,
+      condition,
+      dateOfDiagnosis,
+      dosage,
+      frequency,
+      labResults,
+      sampleCollection,
+    } = state;
+    const code = generateRandomCode(5),
+      date = MyDate();
+
+    const newstate = {
+      code,
+      date,
+      patient: {
+        existingMedical,
+        currentMedication,
+        dateOfDiagnosis,
+        condition,
+        dosage,
+        frequency,
+        labResults,
+        sampleCollection,
+      },
+      details: condition,
+      followups: [],
+      uploaded: false,
+    };
+
+    if (dateOfDiagnosis && existingMedical && condition) {
+      // const data = await AsyncStorage.getItem('@diagnosis')
+      // const prevstate = data !== null ? JSON.parse(data) : []
+      setState({...state, isLoading: true});
+
+      AsyncStorage.setItem(
+        '@diagnosis',
+        JSON.stringify([newstate, ...diagnoses]),
+        () => {
+          diagnosisContext.setDiagnoses([newstate, ...diagnoses]);
+
+          Alert.alert('Saved', `Diagnosis code: ${code}`, [{text: 'OK'}]);
+
+          setState({
+            existingMedical: '',
+            currentMedication: '',
+            condition: '',
+            dateOfDiagnosis: '',
+            dosage: '',
+            frequency: '',
+            sampleCollection,
+            labResults,
+            followups: [],
+            isLoading: false,
+          });
+        },
+      );
+    } else {
+      Alert.alert('Ooops!', 'Complete all fields', [{text: 'OK'}]);
+    }
+  };
+
+  const _header = () => (
+    <CustomHeader
+      left={
+        <TouchableOpacity
+          style={{
+            marginHorizontal: 4,
+            width: 35,
+            height: 35,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={25} color={COLORS.BLACK} />
+        </TouchableOpacity>
+      }
+      title={
+        <Text style={[STYLES.centerHeader, STYLES.title]}>
+          Enter Medical Details
+        </Text>
+      }
+      right={
+        <TouchableOpacity
+          onPress={() => save()}
+          style={{
+            marginHorizontal: 4,
+            width: 35,
+            height: 35,
+            borderRadius: 100,
+            backgroundColor: COLORS.BLACK,
+            borderColor: COLORS.BLACK,
+            borderStyle: 'solid',
+            borderWidth: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Icon name="check" size={25} color={COLORS.WHITE} />
+        </TouchableOpacity>
+      }
+    />
+  );
+
+  if (state.isLoading) return <Loader />;
+
   return (
-    <div>MedicalHistory</div>
-  )
-}
+    <View style={STYLES.wrapper}>
+      <StatusBar backgroundColor={COLORS.WHITE_LOW} barStyle="dark-content" />
 
-export default MedicalHistory
+      {_header()}
+
+      <ScrollView style={STYLES.body} keyboardDismissMode="on-drag">
+        <Text style={STYLES.terms}>Enter Medical Details.</Text>
+        <TextInput
+          style={STYLES.input}
+          autoCorrect={false}
+          placeholderTextColor="rgba(0,0,0,0.7)"
+          selectionColor={COLORS.SECONDARY}
+          onChangeText={text => setState({...state, condition: text})}
+          value={state.condition}
+          placeholder="Condition/Disorder Name"
+        />
+        {/* date of diagnoses */}
+        <TextInput
+          style={STYLES.input}
+          autoCorrect={false}
+          placeholderTextColor="rgba(0,0,0,0.7)"
+          selectionColor={COLORS.SECONDARY}
+          onChangeText={text => setState({...state, dateOfDiagnosis: text})}
+          value={state.dateOfDiagnosis}
+          placeholder="Date of Diagnosis"
+        />
+        {/* drug adminstered */}
+        <TextInput
+          style={STYLES.input}
+          autoCorrect={false}
+          placeholderTextColor="rgba(0,0,0,0.7)"
+          selectionColor={COLORS.SECONDARY}
+          onChangeText={text => setState({...state, currentMedication: text})}
+          value={state.currentMedication}
+          placeholder="Name Of Drug Adminstered"
+        />
+
+        {/* dosage and frequency */}
+        <View style={STYLES.wrap}>
+          <TextInput
+            style={STYLES.detail}
+            autoCorrect={false}
+            placeholderTextColor="rgba(0,0,0,0.7)"
+            selectionColor={COLORS.SECONDARY}
+            onChangeText={text => setState({...state, dosage: text})}
+            value={state.dosage}
+            placeholder="Dosage"
+          />
+          <TextInput
+            style={STYLES.detail}
+            autoCorrect={false}
+            placeholderTextColor="rgba(0,0,0,0.7)"
+            selectionColor={COLORS.SECONDARY}
+            onChangeText={text => setState({...state, frequency: text})}
+            value={state.frequency}
+            placeholder="Frequency"
+          />
+        </View>
+
+        {/* lab results */}
+        <TextInput
+          style={STYLES.input}
+          autoCorrect={false}
+          placeholderTextColor="rgba(0,0,0,0.7)"
+          selectionColor={COLORS.SECONDARY}
+          onChangeText={text => setState({...state, labResults: text})}
+          value={state.labResults}
+          placeholder="Lab Results"
+        />
+
+        {/* date for sample collection */}
+        <TextInput
+          style={STYLES.input}
+          autoCorrect={false}
+          placeholderTextColor="rgba(0,0,0,0.7)"
+          selectionColor={COLORS.SECONDARY}
+          onChangeText={text => setState({...state, sampleCollection: text})}
+          value={state.sampleCollection}
+          placeholder="Date of Sample Collection"
+        />
+
+        <TouchableOpacity
+          style={STYLES.btn}
+          onPress={() => navigation.navigate('Dashboard')}>
+          <Text style={STYLES.btnText}>Next</Text>
+          <Icon
+            name="arrow-right"
+            size={20}
+            strokeSize={3}
+            color={COLORS.WHITE}
+          />
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default PatientMedical;
+
+const STYLES = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: COLORS.WHITE_LOW,
+  },
+  header: {
+    flex: 1,
+  },
+  body: {
+    flex: 2,
+    paddingHorizontal: 20,
+  },
+  alert: {
+    color: COLORS.GREY,
+    textAlign: 'center',
+    marginTop: 15,
+  },
+  subtitle: {
+    flexDirection: 'row',
+    fontSize: 10,
+    color: COLORS.GREY,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  title: {
+    fontWeight: 'bold',
+    color: COLORS.BLACK,
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+  leftHeader: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  centerHeader: {
+    flex: 2,
+    alignItems: 'center',
+    color: COLORS.SECONDARY,
+  },
+  rightHeader: {
+    paddingRight: 10,
+  },
+  tip: {
+    color: 'rgba(0,0,0,0.4)',
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  input: {
+    color: 'rgba(0,0,0,0.7)',
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderColor: COLORS.GREY,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    marginBottom: 10,
+    backgroundColor: COLORS.GREY_LIGHTER,
+  },
+  textarea: {
+    color: 'rgba(0,0,0,0.7)',
+    minHeight: 70,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    borderColor: COLORS.GREY,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    marginBottom: 10,
+    borderRadius: 20,
+  },
+  terms: {
+    paddingVertical: 10,
+    textAlign: 'center',
+    color: 'grey',
+    fontSize: 16,
+    fontWeight: 'bold',
+    paddingVertical: 20,
+  },
+  pickers: {
+    // borderBottomColor: 'rgba(0,0,0,0.7)',
+    // borderBottomWidth:1,
+    borderColor: COLORS.GREY,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: -5,
+    marginBottom: 10,
+  },
+  pickerItemStyle: {
+    color: 'red', // Customize the text color here
+  },
+  labeled: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    color: COLORS.BLACK,
+    marginTop: 10,
+    marginBottom: 10,
+    borderColor: COLORS.GREY,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 20,
+  },
+  label: {
+    flex: 2,
+    color: 'rgba(0,0,0,0.7)',
+  },
+  field: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  submit: {
+    flexDirection: 'row',
+    padding: DIMENS.PADDING,
+    paddingHorizontal: 15,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: COLORS.PRIMARY,
+    borderRadius: 50,
+  },
+  submitText: {
+    color: COLORS.BLACK,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+  wrap: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    minHeight: 70,
+  },
+  detail: {
+    flex: 1,
+    height: 40,
+    marginHorizontal: 8,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: COLORS.GREY,
+    borderRadius: 15,
+    backgroundColor: COLORS.GREY_LIGHTER,
+  },
+  btn: {
+    backgroundColor: COLORS.BLACK,
+    padding: DIMENS.PADDING,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 50,
+    marginTop: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  btnText: {
+    fontSize: 16,
+    alignItems: 'center',
+    fontWeight: '900',
+    justifyContent: 'center',
+    paddingLeft: 40,
+    color: COLORS.WHITE,
+  },
+});
