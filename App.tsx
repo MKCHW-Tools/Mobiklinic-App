d744f3fdfb0dca23736d7e18309bd7c27a60566c
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-06-02 16:11:42 +0300

    Create an instance of Apollo Client in your app's entry point file

--- App.tsx
+++ App.tsx
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from 'react';
 // import {AppRegistry} from 'react-native'
-import Entry from './components/entry'
-import {UserProvider} from './components/providers/User'
-import {DiagnosisProvider} from './components/providers/Diagnosis'
-import { DoctorsProvider } from './components/providers/Doctors'
+import Entry from './components/entry';
+import {UserProvider} from './components/providers/User';
+import {DiagnosisProvider} from './components/providers/Diagnosis';
+import {DoctorsProvider} from './components/providers/Doctors';
 // import {name as appName} from './app.json'
-import SignUp from './components/screens/signup'
-import AppStack from './components/navigators/AppStack'
+import SignUp from './components/screens/signup';
+import AppStack from './components/navigators/AppStack';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 export const App = () => {
-    return (
-        <UserProvider>
-            <DiagnosisProvider>
-                <DoctorsProvider>
-                    <Entry/>
-                </DoctorsProvider>
-            </DiagnosisProvider>
-        </UserProvider>
-        // <Entry/>
-        // <AppStack/>
-    )
-}
+  return (
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <DiagnosisProvider>
+          <DoctorsProvider>
+            <Entry />
+          </DoctorsProvider>
+        </DiagnosisProvider>
+      </UserProvider>
+    </ApolloProvider>
+  );
+};
 
-export default App
+export default App;
 // AppRegistry.registerComponent(appName, () => App)
