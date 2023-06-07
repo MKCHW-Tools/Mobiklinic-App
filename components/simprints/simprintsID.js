a6544217fd66899b0fd38f0f4d9780958c9ea1fc
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-06-07 22:28:42 +0300

    simprints

--- components/simprints/simprintsID.js
+++ components/simprints/simprintsID.js
@@ -1,8 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React, {useCallback, useEffect, useState} from 'react';
+import {DeviceEventEmitter, NativeEventEmitter, Text} from 'react-native';
+// import BeneficiarySelectionScreen from './BeneficiarySelectionScreen';
+
 import {
-  DeviceEventEmitter,
-  NativeEventEmitter,
-  Text,
   Linking,
   Button,
   Alert,
@@ -11,15 +18,7 @@ import {
   useColorScheme,
   NativeModules,
   View,
-  TouchableOpacity,
-  ScrollView,
-  StatusBar,
-  Image,
 } from 'react-native';
-import {COLORS, DIMENS} from '../constants/styles';
-import Icon from 'react-native-vector-icons/Feather';
-import CustomHeader from '../parts/custom-header';
-import {TouchableOpacityBase} from 'react-native';
 
 const {IdentificationModule} = NativeModules;
 const {IdentificationPlus} = NativeModules;
@@ -28,10 +27,10 @@ var OpenActivity = NativeModules.OpenActivity;
 // DeviceEventEmitter.addListener('SimprintsRegistrationSuccess', event => {
 //   const {guid} = event;
 //   console.log(event);
-//   // Alert.alert('Beneficiary Biometrics registered', guid);
+//   Alert.alert('Beneficiary Biometrics registered', guid);
 // });
 
-const SimprintsID = ({navigation}) => {
+const SimprintsID = ({navigation, onNextStep}) => {
   const [identificationPlusResults, setIdentificationPlusResults] = useState(
     [],
   );
@@ -90,6 +89,7 @@ const SimprintsID = ({navigation}) => {
     const userID = 'mpower';
 
     IdentificationModule.triggerIdentification(projectID, moduleID, userID);
+    
   };
 
   const confirmSelectedBeneficiary = () => {
@@ -99,8 +99,10 @@ const SimprintsID = ({navigation}) => {
         selectedUserUniqueId,
       );
     }
-    navigation.navigate('PatientData');
+
     console.log('Beneficiary confirmed');
+    
+    navigation.navigate('PatientData');
   };
 
   useEffect(() => {
@@ -118,291 +120,137 @@ const SimprintsID = ({navigation}) => {
     setEnrollmentGuid(null);
   };
 
-  const _header = () => (
-    <CustomHeader
-      left={
-        <TouchableOpacity
-          style={{
-            marginHorizontal: 4,
-            width: 35,
-            height: 35,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          onPress={() => navigation.goBack()}>
-          <Icon name="arrow-left" size={25} color={COLORS.BLACK} />
-        </TouchableOpacity>
-      }
-    />
-  );
-
   return (
-    <View style={styles.wrapper}>
-      <StatusBar backgroundColor={COLORS.WHITE_LOW} barStyle="dark-content" />
-      {_header()}
-      <View style={styles.wrap}>
-        <Image
-          style={{width: 70, height: 70}}
-          source={require('../imgs/logo.png')}
-        />
-        <Text style={styles.title}>Mobiklinic</Text>
-
-        <View style={styles.container}>
-          {displayMode === 'enrollment' && (
+    <View style={styles.container}>
+      {displayMode === 'enrollment' && (
+        <>
+          {enrollmentGuid && (
             <>
-              {enrollmentGuid && (
-                <>
-                  <Text style={styles.text}>Beneficiary Enrolled on ID:</Text>
-                  <Text style={styles.results}>{enrollmentGuid}</Text>
+              <Text style={styles.text}>Beneficiary Enrolled on ID:</Text>
+              <Text style={styles.text}>{enrollmentGuid}</Text>
+              <View style={{height: 20}} />
+            </>
+          )}
+          <View style={{height: 20}} />
+          <Button
+            title="Register Profile"
+            onPress={() => navigation.navigate('PatientData')}
+          />
+        </>
+      )}
+
+      {displayMode === 'identificationPlus' && (
+        <>
+          {identificationPlusResults.length > 0 && (
+            <React.Fragment key="identification-plus-heading">
+              <Text style={styles.text}>Beneficiary Identified:</Text>
+              <View style={{height: 20}} />
+            </React.Fragment>
+          )}
+
+          {identificationPlusResults
+            .filter(
+              result =>
+                result.confidenceScore >= 50 && result.confidenceScore <= 99,
+            )
+            .map((result, index) => (
+              <View key={index}>
+                <Text>
                   <View style={{height: 20}} />
-                </>
-              )}
+                  Tier: {result.tier}, Confidence: {result.confidenceScore},
+                  Guid: {result.guid}
+                </Text>
+              </View>
+            ))}
+          <View style={{height: 20}} />
+          {showButtons ? (
+            <>
+              <Button
+                title="Confirm Beneficiary"
+                onPress={confirmSelectedBeneficiary}
+              />
               <View style={{height: 20}} />
               <Button
-                title="Go Back"
-                onPress={() => navigation.navigate('PatientData')}
+                title="No Match"
+                onPress={() => setNoMatchButtonPressed(true)}
               />
             </>
+          ) : (
+            <Button
+              title="Go Back"
+              onPress={() => navigation.navigate('PatientData')}
+            />
+          )}
+        </>
+      )}
+
+      {displayMode === 'identification' && (
+        <>
+          {identificationResults.length > 0 && (
+            <React.Fragment key="identification-heading">
+              <Text style={styles.text}>Identification Results:</Text>
+              <View style={{height: 20}} />
+            </React.Fragment>
           )}
 
-          {displayMode === 'identificationPlus' && (
+          {identificationResults
+            .filter(
+              result =>
+                result.confidenceScore >= 50 && result.confidenceScore <= 99,
+            )
+            .map((result, index) => (
+              <View key={index}>
+                <Text style={styles.text}>
+                  <View style={{height: 20}} />
+                  Tier: {result.tier}, Confidence: {result.confidenceScore},
+                  Guid: {result.guid}
+                </Text>
+              </View>
+            ))}
+          <View style={{height: 20}} />
+          {showButtons ? (
             <>
-              {identificationPlusResults.length > 0 && (
-                <React.Fragment key="identification-plus-heading">
-                  <Text style={styles.text}>Beneficiary Identified:</Text>
-                  <View style={{height: 20}} />
-                </React.Fragment>
-              )}
-
-              {identificationPlusResults
-                .filter(
-                  result =>
-                    result.confidenceScore >= 50 &&
-                    result.confidenceScore <= 99,
-                )
-                .map((result, index) => (
-                  <View key={index}>
-                    <View style={{height: 20}} />
-                    <Text style={styles.results}>Tier: {result.tier}</Text>
-                    <Text style={styles.results}>
-                      Confidence: {result.confidenceScore}
-                    </Text>
-                    <Text style={styles.results}>Guid: {result.guid}</Text>
-                  </View>
-                ))}
+              <Button
+                title="Confirm Beneficiary"
+                onPress={confirmSelectedBeneficiary}
+              />
               <View style={{height: 20}} />
-              {showButtons ? (
-                <>
-                  <TouchableOpacity
-                    style={styles.buttonStyle}
-                    onPress={confirmSelectedBeneficiary}>
-                    <Text style={styles.buttonText}> Confirm Beneficiary</Text>
-                    <Icon
-                      name="arrow-right"
-                      size={20}
-                      strokeSize={3}
-                      color={COLORS.WHITE}
-                    />
-                  </TouchableOpacity>
-                  <View style={{height: 20}} />
-                  <TouchableOpacity style={styles.buttonStyle} onPress={goBack}>
-                    <Text style={styles.buttonText}> No Match</Text>
-                    <Icon
-                      name="arrow-right"
-                      size={20}
-                      strokeSize={3}
-                      color={COLORS.WHITE}
-                    />
-                  </TouchableOpacity>
-                </>
-              ) : (
-                <TouchableOpacity
-                  style={styles.buttonStyle}
-                  onPress={() => setNoMatchButtonPressed(true)}>
-                  <Text style={styles.buttonText}> No Match</Text>
-                  <Icon
-                    name="arrow-right"
-                    size={20}
-                    strokeSize={3}
-                    color={COLORS.WHITE}
-                  />
-                </TouchableOpacity>
-              )}
+              <Button
+                title="No Match"
+                onPress={() => setNoMatchButtonPressed(true)}
+              />
             </>
+          ) : (
+            <Button title="Go Back" onPress={goBack} />
           )}
+        </>
+      )}
 
-          {displayMode === 'identification' && (
-            <>
-              {identificationResults.length > 0 && (
-                <React.Fragment key="identification-heading">
-                  <Text style={styles.text}>Identification Results</Text>
-                  <View style={{height: 20}} />
-                </React.Fragment>
-              )}
-
-              {identificationResults
-                .filter(
-                  result =>
-                    result.confidenceScore >= 50 &&
-                    result.confidenceScore <= 99,
-                )
-                .map((result, index) => (
-                  <View key={index}>
-                    <View style={{height: 20}} />
-                    <Text style={styles.results}>Tier: {result.tier}</Text>
-                    <Text style={styles.results}>
-                      Confidence: {result.confidenceScore}
-                    </Text>
-                    <Text style={styles.results}>Guid: {result.guid}</Text>
-                  </View>
-                ))}
-              <View style={{height: 20}} />
-              {showButtons ? (
-                <>
-                  <TouchableOpacity
-                    style={styles.buttonStyle}
-                    onPress={confirmSelectedBeneficiary}>
-                    <Text style={styles.buttonText}>Confirm Beneficiary</Text>
-                    <Icon
-                      name="arrow-right"
-                      size={20}
-                      strokeSize={3}
-                      color={COLORS.WHITE}
-                    />
-                  </TouchableOpacity>
-
-                  <View style={{height: 20}} />
-                  <TouchableOpacity style={styles.buttonStyle} onPress={goBack}>
-                    <Text style={styles.buttonText}>No Match</Text>
-                    <Icon
-                      name="arrow-right"
-                      size={20}
-                      strokeSize={3}
-                      color={COLORS.WHITE}
-                    />
-                  </TouchableOpacity>
-                </>
-              ) : (
-                <TouchableOpacity style={styles.buttonStyle} onPress={goBack}>
-                  <Text style={styles.buttonText}>Go Back</Text>
-                  <Icon
-                    name="arrow-right"
-                    size={20}
-                    strokeSize={3}
-                    color={COLORS.WHITE}
-                  />
-                </TouchableOpacity>
-              )}
-            </>
-          )}
-
-          {!displayMode && (
-            <View style={styles.btnContainer}>
-              <TouchableOpacity
-                style={styles.buttonStyle}
-                onPress={handleIdentification}>
-                <Text style={styles.buttonText}>Start Registration</Text>
-                <Icon
-                  name="arrow-right"
-                  size={20}
-                  strokeSize={3}
-                  color={COLORS.WHITE}
-                />
-              </TouchableOpacity>
-              <View style={{height: 20}} />
-              <TouchableOpacity
-                style={styles.buttonStyle}
-                onPress={handleIdentification}>
-                <Text style={styles.buttonText}>Identify Beneficiary </Text>
-                <Icon
-                  name="arrow-right"
-                  size={20}
-                  strokeSize={3}
-                  color={COLORS.WHITE}
-                />
-              </TouchableOpacity>
-            </View>
-          )}
-        </View>
-      </View>
+      {!displayMode && (
+        <>
+          <Button
+            title="Launch Biometrics to start registration"
+            onPress={handleIdentificationPlus}
+          />
+          <View style={{height: 20}} />
+          <Button title="Identify Beneficiary" onPress={handleIdentification} />
+        </>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    backgroundColor: COLORS.WHITE_LOW,
-  },
   container: {
-    flex: 1,
-    // justifyContent: 'center',
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
-    padding: 20,
-  },
-  centerHeader: {
-    flex: 2,
-    alignItems: 'center',
-    color: COLORS.SECONDARY,
-  },
-  title: {
-    fontWeight: 'bold',
-    color: COLORS.BLACK,
-    alignItems: 'center',
-    flexGrow: 1,
   },
   text: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
-    color: 'rgba(0,0,0,0.7)',
-  },
-  results: {
-    fontSize: 16,
-    color: COLORS.BLACK,
-    fontWeight: 'medium',
-    borderBottomWidth: 1,
-    padding: DIMENS.PADDING,
-    borderBottomColor: '#000',
-  },
-  buttonStyle: {
-    backgroundColor: COLORS.BLACK,
-    padding: DIMENS.PADDING,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderRadius: 50,
-    marginTop: 10,
-    fontWeight: 'bold',
-  },
-  buttonText: {
-    fontSize: 16,
-    padding: DIMENS.PADDING,
-    fontWeight: '900',
-    color: COLORS.WHITE,
-  },
-  wrap: {
-    flex: 2,
-    alignItems: 'center',
-    padding: 25,
-  },
-  title: {
-    color: COLORS.ACCENT_1,
-    fontSize: 20,
-    marginVertical: 10,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    padding: DIMENS.PADDING,
-  },
-  btnContainer: {
-    paddingVertical: 100,
+    color: 'black',
   },
 });
 
