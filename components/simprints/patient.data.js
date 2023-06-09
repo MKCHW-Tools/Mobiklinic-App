b7f06a60d4ebc52cfc340c249b5db275a9565937
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-06-10 00:40:01 +0300

    add styles to patient data form

--- components/simprints/patient.data.js
+++ components/simprints/patient.data.js
@@ -13,15 +13,12 @@ import {
 } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import Icon from 'react-native-vector-icons/Feather';
-import {
-  _removeStorageItem,
-} from '../helpers/functions';
+import {_removeStorageItem} from '../helpers/functions';
 import {DiagnosisContext} from '../providers/Diagnosis';
 import CustomHeader from '../ui/custom-header';
 import Loader from '../ui/loader';
 import DataResultsContext from '../contexts/DataResultsContext';
 import {COLORS, DIMENS} from '../constants/styles';
-
 
 const PatientData = ({navigation}) => {
   const diagnosisContext = React.useContext(DiagnosisContext);
@@ -112,6 +109,18 @@ const PatientData = ({navigation}) => {
     <View style={STYLES.wrapper}>
       {_header()}
       <ScrollView style={STYLES.body}>
+        <Text style={STYLES.terms}>Patient Profile</Text>
+
+        {/* Simprints GUI */}
+        <View style={STYLES.labeled}>
+          <Text style={STYLES.label}>Simprints GUI</Text>
+          <TextInput
+            style={STYLES.field}
+            value={dataResults}
+            onChangeText={text => setState({...state, simprintsGui: text})}
+            placeholder="Enter simprints GUI"
+          />
+        </View>
         {/* First Name */}
         <View style={STYLES.labeled}>
           <Text style={STYLES.label}>First Name:</Text>
@@ -133,104 +142,133 @@ const PatientData = ({navigation}) => {
             placeholder="Enter last name"
           />
         </View>
-
-        {/* Sex */}
-        <View style={STYLES.labeled}>
-          <Text style={STYLES.label}>Sex:</Text>
-          <TextInput
-            style={STYLES.field}
-            value={state.sex}
-            onChangeText={text => setState({...state, sex: text})}
-            placeholder="Enter sex"
-          />
-        </View>
-
-        {/* Age Group */}
-        <View style={STYLES.labeled}>
-          <Text style={STYLES.label}>Age Group:</Text>
-          <TextInput
-            style={STYLES.field}
-            value={state.ageGroup}
-            onChangeText={text => setState({...state, ageGroup: text})}
-            placeholder="Enter age group"
-          />
-        </View>
-
         {/* Phone Number */}
         <View style={STYLES.labeled}>
           <Text style={STYLES.label}>Phone Number:</Text>
           <TextInput
             style={STYLES.field}
             value={state.phoneNumber}
+            keyboardType="phone-pad"
             onChangeText={text => setState({...state, phoneNumber: text})}
             placeholder="Enter phone number"
           />
         </View>
 
-        {/* Weight */}
-        <View style={STYLES.labeled}>
-          <Text style={STYLES.label}>Weight:</Text>
-          <TextInput
-            style={STYLES.field}
-            value={state.weight}
-            onChangeText={text => setState({...state, weight: text})}
-            placeholder="Enter weight"
-          />
+        <View style={STYLES.wrap}>
+          {/* Sex */}
+          <View style={STYLES.detail} placeholderTextColor="rgba(0,0,0,0.7)">
+            <Picker
+              placeholder="Sex"
+              placeholderTextColor={COLORS.BLACK}
+              selectedValue={state.sex}
+              onValueChange={(value, index) => setState({...state, sex: value})}
+              style={STYLES.pickerItemStyle}>
+              <Picker.Item label="Sex" value="Gender" />
+              <Picker.Item label="Female" value="Female" />
+              <Picker.Item label="Male" value="Male" />
+              <Picker.Item label="Other" value="Other" />
+            </Picker>
+          </View>
+          {/* Age Group */}
+          <View style={STYLES.detail} placeholderTextColor="rgba(0,0,0,0.7)">
+            <Picker
+              placeholder="Age"
+              placeholderTextColor={COLORS.BLACK}
+              selectedValue={state.ageGroup}
+              onValueChange={(value, index) =>
+                setState({...state, ageGroup: value})
+              }
+              style={STYLES.pickerItemStyle}>
+              <Picker.Item label="Age" value="Age group" />
+              <Picker.Item label="0 - 3" value="0 - 3" />
+              <Picker.Item label="3 - 10" value="3 - 10" />
+              <Picker.Item label="10 - 17" value="10 - 17" />
+              <Picker.Item label="17 - 40" value="17 - 40" />
+              <Picker.Item label="40 - 60" value="40 - 60" />
+              <Picker.Item label="60 above" value="60 above" />
+            </Picker>
+          </View>
         </View>
 
-        {/* Height */}
-        <View style={STYLES.labeled}>
-          <Text style={STYLES.label}>Height:</Text>
-          <TextInput
-            style={STYLES.field}
-            value={state.height}
-            onChangeText={text => setState({...state, height: text})}
-            placeholder="Enter height"
-          />
+        <View style={STYLES.wrap}>
+          {/* Weight */}
+          <View style={STYLES.detail}>
+            {/* <Text style={STYLES.label}>Weight:</Text> */}
+            <TextInput
+              keyboardType="numeric"
+              value={state.weight}
+              placeholderTextColor={COLORS.BLACK}
+              onChangeText={text => setState({...state, weight: text})}
+              placeholder="Weight (Kgs)"
+            />
+          </View>
+
+          {/* Height */}
+          <View style={STYLES.detail}>
+            {/* <Text style={STYLES.label}>Height:</Text> */}
+            <TextInput
+              keyboardType="numeric"
+              placeholderTextColor={COLORS.BLACK}
+              value={state.weight}
+              onChangeText={text => setState({...state, weight: text})}
+              placeholder="Height (cm)"
+            />
+          </View>
         </View>
 
         {/* District */}
-        <View style={STYLES.labeled}>
-          <Text style={STYLES.label}>District:</Text>
-          <TextInput
-            style={STYLES.field}
-            value={state.district}
-            onChangeText={text => setState({...state, district: text})}
-            placeholder="Enter district"
-          />
+        <View style={STYLES.pickers} placeholderTextColor="rgba(0,0,0,0.7)">
+          <Picker
+            placeholder="District"
+            placeholderTextColor={COLORS.BLACK}
+            selectedValue={state.district}
+            onValueChange={(value, index) =>
+              setState({...state, district: value})
+            }
+            style={STYLES.pickerItemStyle}>
+            <Picker.Item label="District" value="District" />
+            <Picker.Item label="Kampala" value="Kampala" />
+            <Picker.Item label="Buikwe" value="Buikwe" />
+            <Picker.Item label="Jinja" value="Jinja" />
+            <Picker.Item label="Masaka" value="Masaka" />
+            <Picker.Item label="Mbarara" value="Mbarara" />
+          </Picker>
         </View>
-
-        {/* Country */}
-        <View style={STYLES.labeled}>
-          <Text style={STYLES.label}>Country:</Text>
-          <TextInput
-            style={STYLES.field}
-            value={state.country}
-            onChangeText={text => setState({...state, country: text})}
-            placeholder="Enter country"
-          />
+        {/* country */}
+        <View style={STYLES.pickers} placeholderTextColor="rgba(0,0,0,0.7)">
+          <Picker
+            placeholder="Country"
+            placeholderTextColor={COLORS.BLACK}
+            selectedValue={state.country}
+            onValueChange={(value, index) =>
+              setState({...state, country: value})
+            }
+            style={STYLES.pickerItemStyle}>
+            <Picker.Item label="Country" value="Country" />
+            <Picker.Item label="Uganda" value="Uganda" />
+            <Picker.Item label="Kenya" value="Kenya" />
+            <Picker.Item label="Rwanda" value="Rwanda" />
+            <Picker.Item label="Tanzania" value="Tanzania" />
+          </Picker>
         </View>
 
         {/* Primary Language */}
-        <View style={STYLES.labeled}>
-          <Text style={STYLES.label}>Primary Language:</Text>
-          <TextInput
-            style={STYLES.field}
-            value={state.primaryLanguage}
-            onChangeText={text => setState({...state, primaryLanguage: text})}
-            placeholder="Enter primary language"
-          />
-        </View>
-
-        {/* Simprints GUI */}
-        <View style={STYLES.labeled}>
-          <Text style={STYLES.label}>Simprints GUI:</Text>
-          <TextInput
-            style={STYLES.field}
-            value={dataResults}
-            onChangeText={text => setState({...state, simprintsGui: text})}
-            placeholder="Enter simprints GUI"
-          />
+        <View style={STYLES.pickers} placeholderTextColor="rgba(0,0,0,0.7)">
+          <Picker
+            placeholder="Primary Language"
+            placeholderTextColor={COLORS.BLACK}
+            selectedValue={state.primaryLanguage}
+            onValueChange={(value, index) =>
+              setState({...state, primaryLanguage: value})
+            }
+            style={STYLES.pickerItemStyle}>
+            <Picker.Item label="Primary Language" value="Language" />
+            <Picker.Item label="Luganda" value="Luganda" />
+            <Picker.Item label="Lusoga" value="Lusoga" />
+            <Picker.Item label="Runyakore" value="Runyakore" />
+            <Picker.Item label="Rutoro" value="Rutoro" />
+            <Picker.Item label="English" value="English" />
+          </Picker>
         </View>
 
         {/* Submit Button */}
@@ -267,10 +305,11 @@ const STYLES = StyleSheet.create({
     color: COLORS.GREY,
   },
   label: {
-    fontWeight: 'bold',
+    fontWeight: 'medium',
     marginLeft: 5,
     marginRight: 5,
-    color:COLORS.BLACK
+    color: COLORS.BLACK,
+    fontSize:15
   },
   title: {
     fontWeight: 'bold',
@@ -330,10 +369,11 @@ const STYLES = StyleSheet.create({
     borderColor: COLORS.GREY,
     borderStyle: 'solid',
     borderWidth: 1,
-    borderRadius: 20,
+    borderRadius: 10,
     paddingHorizontal: 15,
     marginBottom: 10,
-    backgroundColor: COLORS.GREY,
+    fontWeight: 'bold',
+    // backgroundColor: COLORS.GREY,
   },
   pickerItemStyle: {
     color: 'rgba(0,0,0,0.7)',
@@ -342,34 +382,33 @@ const STYLES = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: 15,
-    paddingVertical: 10,
+    // paddingVertical: 10,
     color: COLORS.BLACK,
     marginTop: 10,
     marginBottom: 10,
     borderColor: COLORS.GREY,
     borderStyle: 'solid',
     borderWidth: 1,
-    borderRadius: 20,
+    borderRadius: 10,
   },
   field: {
     flex: 1,
-    justifyContent: 'flex-end',
-  color:COLORS.BLACK
+    justifyContent: 'center',
+    color: COLORS.BLACK,
   },
   submit: {
-    flexDirection: 'row',
-    padding: DIMENS.PADDING,
-    paddingHorizontal: 15,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: COLORS.PRIMARY,
-    borderRadius: 50,
+    backgroundColor: COLORS.BLACK,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginBottom: 10,
+    marginTop: 10,
   },
   submitText: {
-    color: COLORS.BLACK,
-    textAlign: 'center',
-    textTransform: 'uppercase',
+    color: '#fff',
+    fontSize: 18,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   wrap: {
     flex: 1,
@@ -406,5 +445,40 @@ const STYLES = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 10,
     backgroundColor: COLORS.GREY_LIGHTER,
+  },
+  wrap: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    minHeight: 70,
+  },
+  detail: {
+    flex: 1,
+    paddingHorizontal: 15,
+    // paddingVertical: 10,
+    color: COLORS.BLACK,
+    marginTop: 10,
+    marginBottom: 10,
+    borderColor: COLORS.GREY,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 50,
+    marginHorizontal: 5,
+  },
+  pickerStyle: {
+    flex: 1,
+    paddingHorizontal: 15,
+    // paddingVertical: 10,
+    color: COLORS.BLACK,
+    marginTop: 10,
+    marginBottom: 10,
+    borderColor: COLORS.GREY,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 50,
+    marginHorizontal: 5,
   },
 });
