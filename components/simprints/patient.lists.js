e58a9cb9d5e865999dfc19c8a2a638bfeb82b2cf
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-06-10 17:41:37 +0300

    add style features to get all patient data

--- components/simprints/patient.lists.js
+++ components/simprints/patient.lists.js
@@ -1,20 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {Text, View, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
 import axios from 'axios';
-import { Text, View, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
-import { COLORS, DIMENS } from '../constants/styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {COLORS, DIMENS} from '../constants/styles';
 import Icon from 'react-native-vector-icons/Feather';
 import Loader from '../ui/loader';
+import CustomHeader from '../ui/custom-header';
 
-const PatientLists = () => {
+const PatientLists = ({navigation}) => {
   const [users, setUsers] = useState([]);
+
+  const _header = () => (
+    <CustomHeader
+      left={
+        <TouchableOpacity
+          style={{
+            marginHorizontal: 4,
+            width: 35,
+            height: 35,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={25} color={COLORS.BLACK} />
+        </TouchableOpacity>
+      }
+      title={<Text style={[styles.centerHeader]}>Back</Text>}
+    />
+  );
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get(
-          'https://mobi-be-production.up.railway.app/patients/',
-        );
-        setUsers(response.data);
+        // First, check if the data is stored locally
+        const storedData = await AsyncStorage.getItem('patientList');
+        if (storedData) {
+          setUsers(JSON.parse(storedData));
+        } else {
+          const response = await axios.get(
+            'https://mobi-be-production.up.railway.app/patients/',
+          );
+          setUsers(response.data);
+          // Save the fetched data locally for offline access
+          await AsyncStorage.setItem(
+            'patientList',
+            JSON.stringify(response.data),
+          );
+        }
       } catch (error) {
         console.error(error);
       }
@@ -25,7 +57,7 @@ const PatientLists = () => {
 
   const [expandedUserId, setExpandedUserId] = useState(null);
 
-  const renderUserCard = ({ item }) => {
+  const renderUserCard = ({item}) => {
     const isExpanded = item.id === expandedUserId;
 
     const toggleExpansion = () => {
@@ -39,19 +71,29 @@ const PatientLists = () => {
     return (
       <View style={styles.userCard}>
         <TouchableOpacity onPress={toggleExpansion} style={styles.cardHeader}>
-          <Text style={styles.userName}>{item.firstName} {item.lastName}</Text>
-          <Icon name={isExpanded ? 'chevron-up' : 'chevron-down'} size={25} color={COLORS.PRIMARY} />
+          <Text style={styles.userName}>
+            {item.firstName} {item.lastName}
+          </Text>
+          <Icon
+            name={isExpanded ? 'chevron-up' : 'chevron-down'}
+            size={25}
+            color={COLORS.PRIMARY}
+          />
         </TouchableOpacity>
         {isExpanded && (
           <View style={styles.cardDetails}>
             <Text style={styles.label}>Sex: {item.sex}</Text>
             <Text style={styles.label}>Age Group: {item.ageGroup}</Text>
-            <Text style={styles.label}>Phone Number: {formatPhoneNumber(item.phoneNumber)}</Text>
+            <Text style={styles.label}>
+              Phone Number: {formatPhoneNumber(item.phoneNumber)}
+            </Text>
             <Text style={styles.label}>Weight: {item.weight} kg</Text>
             <Text style={styles.label}>Height: {item.height} cm</Text>
             <Text style={styles.label}>District: {item.district}</Text>
             <Text style={styles.label}>Country: {item.country}</Text>
-            <Text style={styles.label}>Primary Language: {item.primaryLanguage}</Text>
+            <Text style={styles.label}>
+              Primary Language: {item.primaryLanguage}
+            </Text>
             <Text style={styles.label}>Simprints GUI: {item.simprintsGui}</Text>
           </View>
         )}
@@ -59,40 +101,57 @@ const PatientLists = () => {
     );
   };
 
-  const formatPhoneNumber = (phoneNumber) => {
+  const formatPhoneNumber = phoneNumber => {
     // Format the phone number as per your preference
     // Example: Add dashes or parentheses for better readability
     return phoneNumber;
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Patient List</Text>
-      {users.length > 0 ? (
-        <FlatList
-          data={users}
-          keyExtractor={item => item.id.toString()}
-          renderItem={renderUserCard}
-          contentContainerStyle={styles.flatListContent}
-        />
-      ) : (
-        <View><Loader/></View>
-      )}
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        {_header()}
+        <Text style={styles.header}>Patient List</Text>
+        {users.length > 0 ? (
+          <FlatList
+            data={users}
+            keyExtractor={item => item.id.toString()}
+            renderItem={renderUserCard}
+            contentContainerStyle={styles.flatListContent}
+          />
+        ) : (
+          <View>
+            <Loader />
+          </View>
+        )}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 20,
+  },
+  wrapper: {
     flex: 1,
-    padding: DIMENS.PADDING,
+    // padding: DIMENS.PADDING,
     backgroundColor: COLORS.BACKGROUND,
   },
   header: {
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: 'bold',
     color: COLORS.PRIMARY,
     marginBottom: 20,
+    textAlign: 'center',
+    marginVertical: 20,
+    textDecorationLine: 'underline',
+  },
+  centerHeader: {
+    flex: 1,
+    alignItems: 'center',
+    color: COLORS.BLACK,
+    fontWeight: 'bold',
   },
   flatListContent: {
     paddingBottom: 20,
