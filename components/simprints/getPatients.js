5c1c0e5eef646cbf23a82168dbb83e0f6c0227a5
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-06-13 12:13:53 +0300

    get patients for vaccination

--- components/simprints/getPatients.js
+++ components/simprints/getPatients.js
@@ -1,8 +1,7 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {
   View,
   Alert,
-  Switch,
   ScrollView,
   TextInput,
   TouchableOpacity,
@@ -10,6 +9,8 @@ import {
   StyleSheet,
   Image,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {useNavigation} from '@react-navigation/native';
 
 import {
   _removeStorageItem,
@@ -22,35 +23,13 @@ import Loader from '../ui/loader';
 import DataResultsContext from '../contexts/DataResultsContext';
 import {COLORS, DIMENS} from '../constants/styles';
 import CustomHeader from '../ui/custom-header';
-import Icon from 'react-native-vector-icons/Feather';
 
-// const PatientSummary = ({route, navigation}) => {
-const GetPatients = ({navigation}) => {
+const GetPatients = () => {
+  const navigation = useNavigation();
   const {benData} = useContext(DataResultsContext);
-  const [guid, setGuid] = React.useState(
-    benData.length > 0 ? benData[0].guid : '',
-  );
-  const [userData, setUserData] = React.useState(null);
-  const [showConfirmButton, setShowConfirmButton] = React.useState(false);
-
-  const _header = () => (
-    <CustomHeader
-      left={
-        <TouchableOpacity
-          style={{
-            marginHorizontal: 4,
-            width: 35,
-            height: 35,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          onPress={() => navigation.goBack()}>
-          <Icon name="arrow-left" size={25} color={COLORS.BLACK} />
-        </TouchableOpacity>
-      }
-      title={<Text style={[styles.centerHeader]}>Back</Text>}
-    />
-  );
+  const [guid, setGuid] = useState(benData.length > 0 ? benData[0].guid : '');
+  const [userData, setUserData] = useState(null);
+  const [showConfirmButton, setShowConfirmButton] = useState(false);
 
   const fetchData = async () => {
     console.log('GUID:', guid);
@@ -61,16 +40,15 @@ const GetPatients = ({navigation}) => {
       if (response.ok) {
         const data = await response.json();
         setUserData(data);
+        setVaccinations(data.vaccinations); // Set the vaccination data
         setShowConfirmButton(true); // Show the "Confirm Data" button
       } else {
-        // console.error('Error fetching user data:', response.status);
         Alert.alert(
           'Error',
           'Beneficiary not found. Please check the GUID and try again.',
         );
       }
     } catch (error) {
-      console.error('Error fetching user data:', error);
       Alert.alert(
         'Error',
         'Beneficiary not found. Please check the GUID and try again.',
@@ -80,14 +58,24 @@ const GetPatients = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-      {_header()}
+      <CustomHeader
+        left={
+          <TouchableOpacity
+            style={styles.backButton}
+            onPress={() => navigation.goBack()}>
+            <Icon name="arrow-left" size={25} color={COLORS.BLACK} />
+          </TouchableOpacity>
+        }
+        title={<Text style={styles.headerTitle}>Back</Text>}
+      />
+
       <ScrollView style={styles.body}>
         <View style={styles.logo}>
           <Image
-            style={{width: 40, height: 40}}
+            style={styles.logoImage}
             source={require('../imgs/logo.png')}
           />
-          {/* <Text style={styles.title}>Mobiklinic</Text> */}
+          <Text style={styles.logoTitle}>Mobiklinic</Text>
         </View>
 
         <Text style={styles.label}>Simprints GUID</Text>
@@ -100,51 +88,76 @@ const GetPatients = ({navigation}) => {
         {userData && (
           <View style={styles.userData}>
             <Text style={styles.userDataLabel}>
-              First Name {'\t'}
-              <Text style={{fontWeight: 'bold'}}>{userData.firstName}</Text>
+              Full Name: {' \t'}
+              <Text style={styles.userDataValue}>
+                {userData.firstName}
+                {' \t'}
+                {userData.lastName}
+              </Text>
             </Text>
+
             <Text style={styles.userDataLabel}>
-              Last Name:{' \t'}
-              <Text style={{fontWeight: 'bold'}}>{userData.lastName}</Text>
+              Phone Number:{'\t '}
+              <Text style={styles.userDataValue}>{userData.phoneNumber}</Text>
             </Text>
+
             <Text style={styles.userDataLabel}>
-              Sex{'\t'} {'\t'}
-              <Text style={{fontWeight: 'bold'}}>{userData.sex}</Text>
+              Height(Cms):{' '}
+              <Text style={styles.userDataValue}>{userData.height}</Text>
             </Text>
-            <Text style={styles.userDataLabel}>
-              Phone Number {'\t'}
-              <Text style={{fontWeight: 'bold'}}>{userData.phoneNumber}</Text>
-            </Text>
-            <Text style={styles.userDataLabel}>
-              Country {'\t'}
-              {'\t'}
-              <Text style={{fontWeight: 'bold'}}>{userData.country}</Text>
-            </Text>
-            <Text style={styles.userDataLabel}>
-              District {'\t'}
-              <Text style={{fontWeight: 'bold'}}>{userData.district}</Text>
-            </Text>
-            <Text style={styles.userDataLabel}>
-              Age Group: {'\t'}
-              <Text style={{fontWeight: 'bold'}}>{userData.ageGroup}</Text>
-            </Text>
-            <Text style={styles.userDataLabel}>
-              Weight(Kgs): {'\t'}
-              <Text style={{fontWeight: 'bold'}}>{userData.weight}</Text>
-            </Text>
-            <Text style={styles.userDataLabel}>
-              Height(Cms) {'\t'}
-              <Text style={{fontWeight: 'bold'}}>{userData.height}</Text>
-            </Text>
+            {userData.vaccinations && userData.vaccinations.length > 0 && (
+              <View style={styles.vaccinationsContainer}>
+                <Text style={styles.userDataLabel}>Vaccinations:</Text>
+                {userData.vaccinations.map((vaccination, index) => (
+                  <View key={index}>
+                    <Text style={styles.userDataLabel}>
+                      Date of Vaccination:{' '}
+                      <Text style={styles.userDataValue}>
+                        {vaccination.dateOfVaccination}
+                      </Text>
+                    </Text>
+                    <Text style={styles.userDataLabel}>
+                      Date for Next Dose:{' '}
+                      <Text style={styles.userDataValue}>
+                        {vaccination.dateForNextDose}
+                      </Text>
+                    </Text>
+                    <Text style={styles.userDataLabel}>
+                      Vaccine Name:{' \t'}
+                      <Text style={styles.userDataValue}>
+                        {vaccination.vaccineName}
+                      </Text>
+                    </Text>
+                    <Text style={styles.userDataLabel}>
+                      Units:{' '}
+                      <Text style={styles.userDataValue}>
+                        {vaccination.units}
+                      </Text>
+                    </Text>
+                    <Text style={styles.userDataLabel}>
+                      Site Administered:{' '}
+                      <Text style={styles.userDataValue}>
+                        {vaccination.siteAdministered}
+                      </Text>
+                    </Text>
+                    <Text style={styles.userDataLabel}>
+                      Facility:{' '}
+                      <Text style={styles.userDataValue}>
+                        {vaccination.facility}
+                      </Text>
+                    </Text>
+                  </View>
+                ))}
+              </View>
+            )}
           </View>
         )}
-        {showConfirmButton && (
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('SelectActivity')}>
-            <Text style={styles.buttonText}>Confirm Data</Text>
-          </TouchableOpacity>
-        )}
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('SelectActivity')}>
+          <Text style={styles.buttonText}>Confirm Data</Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
@@ -153,78 +166,85 @@ const GetPatients = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // padding: 20,
     backgroundColor: '#fff',
   },
-  body: {
-    flex: 2,
-    paddingHorizontal: 30,
+  backButton: {
+    marginHorizontal: 4,
+    width: 35,
+    height: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-
-  label: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color: COLORS.BLACK,
-    textAlign: 'center',
-    marginTop: 5,
-  },
-  centerHeader: {
+  headerTitle: {
     flex: 1,
     alignItems: 'center',
     color: COLORS.BLACK,
     fontWeight: 'bold',
   },
-  input: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-    color: 'black',
-    justifyContent: 'center',
-    paddingHorizontal: 10,
-  },
-  button: {
-    backgroundColor: COLORS.BLACK,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    marginBottom: 10,
-    marginTop: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  userData: {
-    marginTop: 20,
-  },
-  userDataLabel: {
-    fontSize: 14,
-    marginBottom: 5,
-    color: 'black',
-    borderBottomWidth: 1,
-    padding: DIMENS.PADDING,
-    borderBottomColor: '#000',
-    borderColor: COLORS.GREY,
+  body: {
+    flex: 1,
+    paddingHorizontal: 30,
   },
   logo: {
     justifyContent: 'center',
     alignItems: 'center',
     marginVertical: 20,
   },
-
-  title: {
+  logoImage: {
+    width: 40,
+    height: 40,
+  },
+  logoTitle: {
     color: COLORS.ACCENT_1,
     fontSize: 16,
     marginVertical: 10,
     fontWeight: 'bold',
     textTransform: 'uppercase',
-    padding: DIMENS.PADDING,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: COLORS.PRIMARY,
+  },
+  input: {
+    height: 40,
+    borderColor: COLORS.GRAY,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: COLORS.BLACK,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: COLORS.WHITE,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  userData: {
+    marginBottom: 20,
+  },
+  userDataLabel: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  userDataValue: {
+    fontWeight: 'normal',
+    fontSize: 16,
+  },
+  vaccinationsContainer: {
+    marginTop: 10,
+    paddingLeft: 20,
+    borderLeftWidth: 1,
+    borderLeftColor: COLORS.GRAY,
   },
 });
 
