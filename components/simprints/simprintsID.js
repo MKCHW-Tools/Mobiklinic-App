414ae749c7666d3f1116ee2639ba24bd916f4982
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-06-22 16:16:48 +0300

    change biometrics search

--- components/simprints/simprintsID.js
+++ components/simprints/simprintsID.js
@@ -223,8 +223,8 @@ const SimprintsID = ({navigation}) => {
         <TouchableOpacity
           style={{
             marginHorizontal: 4,
-            width: 35,
-            height: 35,
+            width: 45,
+            height: 45,
             justifyContent: 'center',
             alignItems: 'center',
           }}
@@ -232,7 +232,7 @@ const SimprintsID = ({navigation}) => {
           <Icon name="arrow-left" size={25} color={COLORS.BLACK} />
         </TouchableOpacity>
       }
-      title={<Text style={[styles.centerHeader]}>Biometrics Integration</Text>}
+      title={<Text style={[styles.centerHeader]}>Biometrics Identification</Text>}
     />
   );
 
@@ -441,7 +441,7 @@ const SimprintsID = ({navigation}) => {
                                           </Text>
                                         </Text>
                                         <Text style={styles.userDataLabel}>
-                                          .................................................................{' '}
+                                          .................................................................
                                           <Text
                                             style={styles.userDataValue}></Text>
                                         </Text>
@@ -514,7 +514,7 @@ const SimprintsID = ({navigation}) => {
                                           </Text>
                                         </Text>
                                         <Text style={styles.userDataLabel}>
-                                          .................................................................{' '}
+                                          .................................................................
                                           <Text
                                             style={styles.userDataValue}></Text>
                                         </Text>
@@ -645,6 +645,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     color: COLORS.BLACK,
     fontWeight: 'bold',
+    fontSize: 16,
   },
   button: {
     backgroundColor: COLORS.BLACK,
