f05d6ec01975fdaced5530fcfdbd950e72c4fbbf
Arafat <mobi@Mobis-MacBook-Pro.local>
2023-06-22 16:09:41 +0300

    Resume chnages

--- components/simprints/patient.lists.js
+++ components/simprints/patient.lists.js
@@ -1,16 +1,24 @@
-import React, {useEffect, useState, useContext} from 'react';
-import {Text, View, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
+import React, { useEffect, useState, useContext } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  RefreshControl,
+} from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {COLORS, DIMENS} from '../constants/styles';
+import { COLORS, DIMENS } from '../constants/styles';
 import Icon from 'react-native-vector-icons/Feather';
 import Loader from '../ui/loader';
 import CustomHeader from '../ui/custom-header';
 import DataResultsContext from '../contexts/DataResultsContext';
 
-const PatientLists = ({navigation}) => {
+const PatientLists = ({ navigation }) => {
   const [users, setUsers] = useState([]);
-  const {userLog} = useContext(DataResultsContext); // Get the logged-in user ID from the context
+  const [refreshing, setRefreshing] = useState(false);
+  const { userLog } = useContext(DataResultsContext); // Get the logged-in user ID from the context
 
   const _header = () => (
     <CustomHeader
@@ -23,7 +31,8 @@ const PatientLists = ({navigation}) => {
             justifyContent: 'center',
             alignItems: 'center',
           }}
-          onPress={() => navigation.goBack()}>
+          onPress={() => navigation.goBack()}
+        >
           <Icon name="arrow-left" size={25} color={COLORS.BLACK} />
         </TouchableOpacity>
       }
@@ -31,36 +40,40 @@ const PatientLists = ({navigation}) => {
     />
   );
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        // First, check if the data is stored locally
-        const storedData = await AsyncStorage.getItem('patientList');
-        if (storedData) {
-          setUsers(JSON.parse(storedData));
-        } else {
-          const response = await axios.get(
-            `https://mobi-be-production.up.railway.app/${userLog}/patients`, // Use the logged-in user ID in the API URL
-          );
-          setUsers(response.data);
-          // Save the fetched data locally for offline access
-          await AsyncStorage.setItem(
-            'patientList',
-            JSON.stringify(response.data),
-          );
-        }
-      } catch (error) {
-        console.error(error);
+  const fetchUsers = async () => {
+    try {
+      // First, check if the data is stored locally
+      const storedData = await AsyncStorage.getItem('patientList');
+      if (storedData) {
+        setUsers(JSON.parse(storedData));
+      } else {
+        const response = await axios.get(
+          `https://mobi-be-production.up.railway.app/${userLog}/patients`, // Use the logged-in user ID in the API URL
+        );
+        setUsers(response.data);
+        // Save the fetched data locally for offline access
+        await AsyncStorage.setItem('patientList', JSON.stringify(response.data));
       }
-    };
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setRefreshing(false);
+    }
+  };
 
+  useEffect(() => {
     fetchUsers();
   }, [userLog]);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    fetchUsers();
+  };
 
   const [expandedUserId, setExpandedUserId] = useState(null);
   const [vaccinationData, setVaccinationData] = useState(null);
 
-  const renderUserCard = ({item}) => {
+  const renderUserCard = ({ item }) => {
     const isExpanded = item.id === expandedUserId;
 
     const toggleExpansion = () => {
@@ -93,7 +106,7 @@ const PatientLists = ({navigation}) => {
             <Text style={styles.label}>
               Primary Language: {item.primaryLanguage}
             </Text>
-            <Text style={styles.label}>Simprints GUI: {item.simprintsGui}</Text>
+
             {item.vaccinations && item.vaccinations.length > 0 && (
               <View>
                 {item.vaccinations.map((vaccination, index) => (
@@ -104,14 +117,8 @@ const PatientLists = ({navigation}) => {
                     <Text style={styles.label}>
                       Vaccination Date: {vaccination.dateOfVaccination}
                     </Text>
-                    <Text style={styles.label}>
-                      {' '}
-                      Dosage: {vaccination.dose}
-                    </Text>
-                    <Text style={styles.label}>
-                      {' '}
-                      Units: {vaccination.units}
-                    </Text>
+                    <Text style={styles.label}> Dosage: {vaccination.dose}</Text>
+                    <Text style={styles.label}> Units: {vaccination.units}</Text>
                   </View>
                 ))}
               </View>
@@ -122,7 +129,7 @@ const PatientLists = ({navigation}) => {
     );
   };
 
-  const formatPhoneNumber = phoneNumber => {
+  const formatPhoneNumber = (phoneNumber) => {
     return phoneNumber;
   };
 
@@ -130,13 +137,16 @@ const PatientLists = ({navigation}) => {
     <View style={styles.wrapper}>
       <View style={styles.container}>
         {_header()}
-        <Text style={styles.header}>Beneficary List</Text>
+        <Text style={styles.header}>Beneficiary List</Text>
         {users.length > 0 ? (
           <FlatList
             data={users}
-            keyExtractor={item => item.id.toString()}
+            keyExtractor={(item) => item.id.toString()}
             renderItem={renderUserCard}
             contentContainerStyle={styles.flatListContent}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
           />
         ) : (
           <View>
