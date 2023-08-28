dd6fc3fd0b65e96701a3599aff7cded2a96a0a39
Silas281 <sangminsilas@gmail.com>
2023-08-28 11:52:54 +0000

    Added tests for validation functions

--- components/components_v1/functions/functions.test.js
+++ components/components_v1/functions/functions.test.js
@@ -8,19 +8,5 @@ it('should return a string with the current date and time in the expected format
     expect(result).toEqual(expected);
 });
 
-// // Tests that the function handles time zones correctly
-// it('should handle time zones correctly', () => {
-//     const expected = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
-//     const result = MyDate(new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }));
-//     expect(result).toEqual(expected);
-// });
 
-// // Tests that the function returns a string with the correct date and time even if the system clock is changed during execution
-// it('should return a string with the correct date and time even if the system clock is changed during execution', () => {
-//     const expected = new Date().toLocaleString();
-//     const result = MyDate();
-//     jest.spyOn(global, 'Date').mockImplementation(() => new Date(2021, 5, 1, 12, 0, 0));
-//     expect(result).toEqual(expected);
-//     global.Date.mockRestore();
-// });
 
--- components/components_v1/functions/validation.test.js
+++ components/components_v1/functions/validation.test.js
@@ -1,9 +1,81 @@
-import { validateAlphaNum, similarPasswords } from "../../helpers/validation";
+import { validateAlphaNum, validateNumber, validateUgandaPhoneNumber, validateEmail, similarPasswords, validatePassword } from "../../helpers/validation";
+
+//Testing validation functions
+
+//validateAlphaNum
+describe('validateAlphaNum', () => {
+    it('should return true for an alphanumeric string', () => {
+        expect(validateAlphaNum('hello123')).toBeTruthy();
+    });
+
+    it('should return false for a string that is not alphanumeric', () => {
+        expect(validateAlphaNum('12345323455')).toBeFalsy();
+    });
+
+    it('should return false for a string that empty', () => {
+        expect(validateAlphaNum('')).toBeFalsy();
+    });
+});
+
+//validateNumber
+describe('validateNumber', () => {
+    it('should return true for a numeric string', () => {
+        expect(validateNumber('1234567890')).toBeTruthy();
+    });
+
+    it('should return false for a string that is not numeric', () => {
+        expect(validateNumber('hello123')).toBeFalsy();
+    });
+
+    it('should return false for a string that empty', () => {
+        expect(validateNumber('')).toBeFalsy();
+    });
+});
+
+//validateUgandaPhoneNumber
+describe('validateUgandaPhoneNumber', () => {
+    it('should return true for a valid Uganda phone number', () => {
+        expect(validateUgandaPhoneNumber('256772123456')).toBeTruthy();
+    });
+
+    it('should return false for an invalid Uganda phone number', () => {
+        expect(validateUgandaPhoneNumber('25677212345')).toBeFalsy();
+    });
+});
+
+//validateEmail
+describe('validateEmail', () => {
+    it('should return true for a valid email address', () => {
+        expect(validateEmail('hello@world.com')).toBeTruthy();
+    });
+
+    it('should return false for an invalid email address', () => {
+        expect(validateEmail('helloworld.com')).toBeFalsy();
+    });
+
+    it('should return false for an empty email address', () => {
+        expect(validateEmail('')).toBeFalsy();
+    });
+});
+
+//validatePassword
+describe('validatePassword', () => {
+    it('should return true for a valid password', () => {
+        expect(validatePassword('Hello123@')).toBeTruthy();
+    });
+
+    it('should return false for an invalid password', () => {
+        expect(validatePassword('hello123')).toBeFalsy();
+    });
+
+    it('should return false for an empty password', () => {
+        expect(validatePassword('')).toBeFalsy();
+    });
+});
 
 
 
-
-
+//SimilarPasswords
 
 // Tests that the function returns true when two identical passwords are inputted
 it("should test if two passwords are equal", () => {
